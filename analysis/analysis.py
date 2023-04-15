4ab05ec88c1b17e98818ac30fdc9c757db84a1f1
zythenoob <jiayezhu@usc.edu>
2023-04-14 18:08:59 -0700

    analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -11,28 +11,26 @@ from trainer.analysis.utils import fig2img
 from distributed import ParallelConfig
 
 
-def plot_lines(path, ep, t_r, spd_r, spd_u, pd_r, pd_u):
-    spd_r_mean = np.mean(spd_r, axis=1)
-    spd_r_std = np.std(spd_r, axis=1)
-    pd_r_mean = np.mean(pd_r, axis=1)
-    pd_r_std = np.std(pd_r, axis=1)
-    df = pd.DataFrame({
-        'spd_r_mean': spd_r_mean, 'spd_r_std': spd_r_std, 'spd_u': spd_u, 
-        'pd_r_mean': pd_r_mean, 'pd_r_std': pd_r_std, 'pd_u': pd_u, 
-    }, index=ep)
-    print(df)
+def plot_lines(path, ep, t_r, updates, rewards):
+
     fig = plt.figure(figsize=(7, 4))  # Create matplotlib figure
     plt.tight_layout()
     ax = fig.add_subplot(111)  # Create matplotlib axes
     ax2 = ax.twinx()
     ax.axhline(y=t_r, color='r', linestyle='dotted', label="Teacher reward")
 
-    df.spd_u.plot(kind='line', color='coral', ax=ax2, label="SPD updates")
-    df.pd_u.plot(kind='line', color='red', ax=ax2, label="PD updates")
-    df.spd_r_mean.plot(kind='line', ax=ax, label="SPD reward")
-    df.pd_r_mean.plot(kind='line', color='green', ax=ax, label="PD reward")
-    ax.fill_between(ep, (df.spd_r_mean-df.spd_r_std), (df.spd_r_mean+df.spd_r_std), color='b', alpha=.1)
-    ax.fill_between(ep, (df.pd_r_mean-df.pd_r_std), (df.pd_r_mean+df.pd_r_std), color='g', alpha=.1)
+    for k in updates.keys():
+        if len(updates[k][0]) == 0:
+            continue
+        name = k
+        reward_mean = np.mean(rewards[name], axis=1)
+        reward_std = np.std(rewards[name], axis=1)
+        update_mean = np.mean(updates[name], axis=1)
+        # rewards
+        ax.plot(ep, reward_mean, label=f'{name} rewards')
+        ax.fill_between(ep, reward_mean-reward_std, reward_mean+reward_std, alpha=.1)
+        # updates
+        ax2.plot(ep, update_mean, linestyle='dashed', label=f'{name} updates')
 
     ax.set_ylabel('Average Reward', fontsize=12)
     ax2.set_ylabel('Num updates', fontsize=12)
@@ -55,47 +53,46 @@ def plot_lines(path, ep, t_r, spd_r, spd_u, pd_r, pd_u):
 if __name__ == "__main__":
     # spd_experiment_dir = Path("/home/ji/experiments/hopper_spd/mp_run_3456_8ff5")
     # pd_experiment_dir = Path("/home/ji/experiments/hopper_pd/mp_run_dcb9_a891")
-    spd_experiment_dir = Path("/home/ji/experiments/walker_spd/mp_run_98d9_82fa")
-    pd_experiment_dir = Path("/home/ji/experiments/walker_pd/mp_run_e125_9c2c")
-    run_configs = list(spd_experiment_dir.rglob("run_config.yaml")) + list(pd_experiment_dir.rglob("run_config.yaml"))
+    experiment_dir = Path("/home/ji/experiments/walker")
+    # experiment_dir = Path("/home/ji/experiments/mp_run_d9a2_4a5b")
+    run_configs = list(experiment_dir.rglob("run_config.yaml"))
     results_dir = Path("/home/ji/experiments/").joinpath("analysis")
 
     # fig, ax = plt.subplots()
     episodes = np.arange(100) + 1
-    spd_updates = [0 for _ in range(100)]
-    pd_updates = [0 for _ in range(100)]
-    spd_reward = [[] for _ in range(100)]
-    pd_reward = [[] for _ in range(100)]
+    updates = {}
+    rewards = {}
     teacher_reward = []
 
     for run_config in run_configs:
+        if 'mp_run_6d4f_8a13' in run_config.as_posix():
+            continue
         config = ParallelConfig.load(run_config)
         method_name = config.model_config.name
         try:
             results = CustomReport(
                 config=config,
                 parse_informative=False,
+            )
+
+            results = CustomReport(
+                config=config,
+                parse_informative=False,
             ).results
 
-            # print(results.columns)
-            # print(len(results))
+            updates[method_name] = [[] for _ in range(100)]
+            rewards[method_name] = [[] for _ in range(100)]
 
             for i, row in results.iterrows():
                 train_ep = int(row['train_aux_traj_names'])
-                updates = eval(method_name + '_updates')
-                rewards = eval(method_name + '_reward')
-                updates[train_ep] += int(row['num_updates'])
-                rewards[train_ep].append(float(row['val_reward']))
+                updates[method_name][train_ep].append(float(row['num_updates']))
+                rewards[method_name][train_ep].append(float(row['val_reward']))
                 teacher_reward.append(float(row['train_reward']))
 
         except Exception as e:
             traceback.print_exc()
             raise e
         
-    spd_reward = np.array(spd_reward)
-    spd_updates = np.array(spd_updates) / 20
-    pd_reward = np.array(pd_reward)
-    pd_updates = np.array(pd_updates) / 20
     teacher_reward = np.mean(teacher_reward)
 
-    plot_lines(results_dir, episodes, teacher_reward, spd_reward, spd_updates, pd_reward, pd_updates)
+    plot_lines(results_dir, episodes, teacher_reward, updates, rewards)
